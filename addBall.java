4f4b1795af6b1acf9c00c5e515005736686b5f73
deburca98 <62703856+deburca98@users.noreply.github.com>
2021-04-28 00:19:12 +0100

    Update addBall.java

--- addBall.java
+++ addBall.java
@@ -1,10 +1,19 @@
 package sandbox;
 
 import java.awt.Graphics;
+import java.util.Random;
 import javax.swing.JPanel;
 
-public class addBall extends JPanel   {
-	int x=0, y=0;
+
+public class Ball extends JPanel   {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// I imported this class to create the random position 
+	// creates a random position for the ball on startup
+	private Random randomGenerator = new Random();
+	int x=0 + randomGenerator.nextInt( 201 ), y=0+ randomGenerator.nextInt( 201 );
 	int anglex =1, angley =1;
 	
 	
@@ -12,11 +21,11 @@ public class addBall extends JPanel   {
 	void move(){
 		if(x + anglex <0) {
 			anglex=1;
-		}else if (x +anglex > getWidth()-50) {
+		}else if (x +anglex > getWidth()-50) { // used library getwidth instead
 			anglex =-1;
 		} else if (y + angley <0) {
 			angley = 1;
-		} else if (y + angley > getHeight() -50) {
+		} else if (y + angley > getHeight() -50) { 
 			angley = -1;
 		}
 			x =x + anglex;
@@ -25,7 +34,9 @@ public class addBall extends JPanel   {
 	@Override
 	public void paint (Graphics g) {
 		super.paint(g);
-		g.fillOval(x, y, 50, 50); // ball is set to 50 by 50
+		g.fillOval(x, y , 50, 50); // ball is set to 50 by 50
+		
+	
 	}
 
 	
